3b64c407e3dd851ad6075fb6d990845470e1351e
ishemapro <ishetou.123@gmail.com>
2019-08-07 17:40:15 +0200

    ff

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var http = require('http');
-
+var port= process.env.PORT || 8080;
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
 
   res.end('<h1> my name is la douce</h1> ');
-}).listen(1101);
-console.log("I'm running on port "+1101);
+}).listen(port);
+console.log("I'm running on port "+port);
